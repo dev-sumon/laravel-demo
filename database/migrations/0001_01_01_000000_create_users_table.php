8762fa26c48557784a66200c82bf416755b1f410
Md Akhtaruzzaman Sumon <146063221+dev-sumon@users.noreply.github.com>
2024-05-27 01:42:08 +0600

    Authentication done

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->index('users_email_unique', '100');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
