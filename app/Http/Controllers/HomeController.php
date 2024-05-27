fc04c6053a0f3a36d16eb9bb35c389248acf2a1f
Md Akhtaruzzaman Sumon <146063221+dev-sumon@users.noreply.github.com>
2024-05-27 20:45:17 +0600

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['login', 'logout']);
     }
 
     /**
@@ -24,5 +25,46 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    /**
+     * Handle the login request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
+    public function login(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+            if (Auth::guard('admin')->attempt(['email' => $data['email'], 'password' => $data['password']])) {
+                return redirect('layouts.backend_master');
+            } else {
+                return redirect()->back()->with('error_message', 'Invalid Email or Password');
+            }
+        }
+
+        return view('include.backend.login');
+    }
+
+    /**
+     * Handle the logout request.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect('include.backend.login');
+    }
+
+    /**
+     * Show the dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function dashboard()
+    {
+        return view('layouts.backend_master');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/login', [App\Http\Controllers\HomeController::class, 'login'])->name('login');
+Route::get('/logout', [App\Http\Controllers\HomeController::class, 'logout'])->name('logout');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard');
+
+
+
+// Route::get('/login', function () {
+//     return view('login');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('layouts.backend_master');
+// });
